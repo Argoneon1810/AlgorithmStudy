37e84f6d231ab072fc5225481f076b6da83fe335
Jinhoon Choi <62126566+Argoneon1810@users.noreply.github.com>
2026-08-06 05:52:07 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 134 ms, Memory: 29,000 KB -BaekjoonHub

--- Argoneon1810/SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
+++ Argoneon1810/SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
@@ -1,90 +1,116 @@
 import java.io.BufferedReader;
-// import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
 import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
 
 class Solution
 {
-	static final int deltasXY[][] = {
-			{0, -1},
-			{0, 1},
-			{-1, 0},
-			{1, 0}
-	};
-	static String solve(BufferedReader br) throws IOException {
-		// 임의의 위치에서 시작해서 6번의 이동을 통해 구성 가능한 모든 7자리 숫자 조합 찾기 
-		// 4x4라서 보드가 크진 않다
-		// 단 한번 방문한 칸을 재방문 해도 된다는 조건이 있어 재귀로 풀면 재귀 깊이 문제가 발생
-		// 어떤 칸은 자신이 N번째로 방문되었을 때 뱉을 수 있는 문자열이 동일하다. = 메모리제이션 가능
-		// 자기 자신을 기준으로 한 칸 이동으로 갈 수 있는 블럭 = 4방향 블럭
-		// 이동하지 않아도 되는가? X
-		// T-1스텝의 4방향으로부터 숫자 조합을 받고 그 다음 내 숫자 붙이기.
-		// - "0으로 시작하는 0102001과 같은 수를 만들 수도 있다"라고 했으니 내 숫자는 다른 숫자 앞에 붙는다
-		// - "0으로 시작하는 값"이 나와야 한다는 거니까 문자열로 관리 = 4x4(격자)x7(깊이)x4(방향) 정도면 그냥 String Concat 하자.
-		// T=7일 때의 모든 셀의 값을 출력
-		// 중복값이 있으면?
-		// - 모든 셀이 Set을 가지고 있게 하자. 로컬에서 중복이 있으면 미리 걸러질거고, 최종 4x4도 Set에 넣고 빼면 된다.
-		// 계획 10분
-		final int N = 4;
-		byte board[][] = new byte[N][N];
-		for (int j=0; j<N; ++j) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i=0; i<N; ++i)
-				board[j][i] = Byte.parseByte(st.nextToken());
-		}
-		final int D = 7; // depth
-		List<ArrayList<HashSet<String>>> prev = null, curr = null;
-		// 한번 연산하고 지나갔다면 그 전 단계는 불필요하므로 prev와 curr만 유지.
-		// 마지막에 curr만 읽으면 됨
-		for (int o=0; o < D; ++o) {
-			curr = new ArrayList<>();
-			for (int j=0; j<N; ++j) {
-				ArrayList<HashSet<String>> outer = new ArrayList<>();
-				for (int i=0; i<N; ++i) {
-					HashSet<String> inner = new HashSet<>();
-					// body가 여기에. 여기까지 37분
-					if (o==0)
-						inner.add(Integer.toString(board[j][i]));
-					else {
-						for(int d=0; d<4; ++d) {
-							int deltaXY[] = deltasXY[d];
-							int tx, ty;
-							tx = i+deltaXY[0];
-							ty = j+deltaXY[1];
-							if (tx < 0 || ty < 0 || tx >= N || ty >= N)
-								continue;
-							HashSet<String> prevs = prev.get(ty).get(tx);
-							for(String perm : prevs)
-								inner.add(Integer.toString(board[j][i]) + perm);
-						}
-					}
-					// body 작성 끝. 여기까지 45분
-					outer.add(inner);
-				}
-				curr.add(outer);
-			}
-			prev = curr;
-		}
-		Set<String> lastCheck = new HashSet<>();
-		for (int j=0; j<N; ++j)
-			for (int i=0; i<N; ++i)
-				lastCheck.addAll(curr.get(j).get(i));
-		return Integer.toString(lastCheck.size());
-		// 예시 input 기준 23 나오는 것 확인. 여기까지 48분. 
-	}
+	static final int DN;
+	static final int deltaX[];
+	static final int deltaY[];
 	
-	public static void main(String args[]) throws Exception
-	{
-		// System.setIn(new FileInputStream("./src/com/swea/q2819/res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T = Integer.parseInt(br.readLine().trim());
-		for(int test_case = 1; test_case <= T; test_case++)
-			System.out.println(String.format("#%d %s", test_case, solve(br)));
-	}
+	final int N;
+	final int DEPTH;
+	final int MAX_PATHS;
+	
+	final byte board[];
+	final int results[];
+	int resultCount;
+	
+    static {
+    	DN = 4;
+    	deltaX = new int[] {0, 0, -1, 1};
+    	deltaY = new int[] {-1, 1, 0, 0};
+    }
+    
+    {
+    	N = 4;
+    	DEPTH = 7;
+    	MAX_PATHS = (N*N) * ((int) Math.pow(DN, DEPTH-1)); // 4x4 board * 4 deltas ^ 6 sub-depths 
+    	
+    	board = new byte[N*N];
+    	results = new int[MAX_PATHS];
+    	resultCount = 0;
+    }
+    
+    byte[] getBoard(BufferedReader br) throws IOException {
+        for (int j=0; j<N; ++j) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i=0; i<N; ++i)
+                board[j*N+i] = Byte.parseByte(st.nextToken());
+        }
+        return board;
+    }
+    
+    void solveRecursive(int pos, int depth, int value) {
+    	value = value*10+board[pos];
+    	if (depth >= DEPTH-1) {
+    		results[resultCount++] = value;
+    		return;
+    	}
+    	int prevY = pos / N;
+    	int prevX = pos % N;
+    	for (int i=0; i<DN; ++i) {
+    		int currY = prevY + deltaY[i];
+    		int currX = prevX + deltaX[i];
+    		if (currY >= N || currX >= N || currY < 0 || currX < 0)
+    			continue;
+    		solveRecursive(currY*N+currX, depth+1, value);
+    	}
+    }
+    
+    String solveInner(BufferedReader br) throws IOException {
+    	// 개선시도 v2
+    	// 1. 순회순서 문제로 인한 캐시 미스 문제를 겪어 보았으니, board에도 직렬화를 적용해보자
+    	// 2. 수열 그 자체를 인덱스로 하는 배열은 지나치게 sparse하다.
+    	//	  메모리에서도, 순회길이에서도 전부 열등하다.
+    	// 3. 애초에 100_000_000 길이의 배열 그 자체가 유연성이 없다.
+    	//	  문제 내용에 딱 맞춘 해킹 그 자체인데, 아무 유익도 얻지 못했다.
+    	
+    	// 계획 3번까지 + 초기 상태 형성 18분 53초
+        getBoard(br);
+        
+        // 재귀로 해보자.
+        // y*N+x번째 칸을 여러번 방문할 것이지만, 방문했을 때의 depth가 무엇인지에 따라 반환값이 결정론적이라는 말은
+        // 그냥 재귀해서 deepest node에 도달했을 때 자기 값을 반환시키고 부모 노드에서 자식 노드의 값을 자기 뒤에 append 시키겠다는 말과 같다
+        // 즉 그냥 완전탐색 하는 것과 다를게 없다
+        
+    	resultCount = 0;
+    	for(int i=0; i<N*N; ++i)
+    		solveRecursive(i, 0, 0);
+    	
+    	// 중복체크는 어떻게 할까
+    	// Set에 배열 전부 넣기 vs 정렬하고 값이 달라질때만 카운터 올리기
+    	
+//    	// Set에 배열 전부 넣기
+//    	// 이거 안되네
+//    	Set<Integer> s = new HashSet<Integer>();
+//    	Collections.addAll(s, Arrays.copyOf(results, resultCount));
+//    	return Integer.toString(s.size());
+    	
+    	// 정렬하고 값이 달라질때만 카운터 올리기
+    	Arrays.sort(results, 0, resultCount);
+    	int distinct = 0;
+        for (int i = 0; i < resultCount; ++i)
+            if (i == 0 || results[i] != results[i - 1])
+                ++distinct;
+        return Integer.toString(distinct);
+    }
+    
+    void solveOuter() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T = Integer.parseInt(br.readLine().trim());
+        for(int test_case = 1; test_case <= T; test_case++)
+            System.out.println(String.format("#%d %s", test_case, solveInner(br)));
+    }
+     
+    public static void main(String args[]) throws Exception
+    {
+    	new Solution().solveOuter();
+    }
 }
