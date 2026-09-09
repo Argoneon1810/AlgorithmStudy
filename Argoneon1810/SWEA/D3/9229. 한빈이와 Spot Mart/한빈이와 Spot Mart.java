a528586ea42dda79e987cdd6c516e34e78d47f91
Jinhoon Choi <62126566+Argoneon1810@users.noreply.github.com>
2026-09-09 16:42:36 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 163 ms, Memory: 33,408 KB -BaekjoonHub

--- Argoneon1810/SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ Argoneon1810/SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -5,29 +5,47 @@ import java.util.StringTokenizer;
 import java.util.Arrays;
 
 class Solution {
+	int dfs(
+			int[] weight, boolean[] visited,
+			int numSnacks, int maxWeightLimit,
+			int accumWeight,
+			int consideredSnacks, int takenSnacks
+	) {
+		if (takenSnacks == 2) return accumWeight;
+		if (consideredSnacks >= numSnacks) return -1;
+		int currentWeight = weight[consideredSnacks];
+		int tempAccumWeight = accumWeight + currentWeight;
+		boolean visitable = tempAccumWeight <= maxWeightLimit;
+		int a=-1;
+		if (visitable) {
+			a = dfs(weight, visited, numSnacks, maxWeightLimit,
+					tempAccumWeight, consideredSnacks+1, takenSnacks+1);
+			if (a<0) return -1;
+		}
+		int b = dfs(weight, visited, numSnacks, maxWeightLimit,
+				accumWeight, consideredSnacks+1, takenSnacks);
+		int m = Integer.max(a, b); 
+		visited[consideredSnacks] = (m==a);
+		boolean took2 = takenSnacks == 2;
+		boolean lightEnough = (m <= maxWeightLimit);
+		if (!took2 && !lightEnough)
+			return -1;
+		return m;
+	}
+	
 	String solveInner(BufferedReader br) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N, M;
 		N = Integer.parseInt(st.nextToken());		// [2, 1000]
 		M = Integer.parseInt(st.nextToken());		// [2, 2000000]
 		int[] weight = new int[N];
+		boolean[] visited = new boolean[N]; 
 		st = new StringTokenizer(br.readLine());
 		for (int i=0; i<N; ++i)
 			weight[i] = Integer.parseInt(st.nextToken());
-		Arrays.sort(weight); 
-		int i = 0, j = N-1;
-		int twBest = Integer.MIN_VALUE;
-		while (i != j) {
-			int tw = weight[i] + weight[j];
-			if (tw <= M) {
-				++i;
-				twBest = Integer.max(tw, twBest);
-				continue;
-			}
-			else if (tw > M)
-				--j;
-		}
-		return Integer.toString((twBest>=0) ? twBest : -1);
+		Arrays.sort(weight);
+		int m = dfs(weight, visited, N, M, 0, 0, 0);
+		return Integer.toString(m == 0 ? -1 : m);
 	}
 
 	void solve() throws IOException {
