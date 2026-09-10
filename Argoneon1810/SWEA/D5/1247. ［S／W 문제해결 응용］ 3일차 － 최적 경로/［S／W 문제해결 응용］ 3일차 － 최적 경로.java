445b6db6b457ae28fad5b258a868246d07aa4638
Jinhoon Choi <62126566+Argoneon1810@users.noreply.github.com>
2026-09-10 17:42:55 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 93 ms, Memory: 26,752 KB -BaekjoonHub

--- Argoneon1810/SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ Argoneon1810/SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -7,57 +7,49 @@ import java.util.StringTokenizer;
 class Solution {
 	static final int WORK = 0;
 	static final int HOME = 1;
+	static final int FIRST_CUSTOMER = 2;
 	static final int X = 0;
 	static final int Y = 1;
-	static final int UNKNOWN = -1;
+	static final int INF = Integer.MAX_VALUE;
 
-	int getMemoL1Distance(
-			int[][] memoDistances,
-			int[][] posArray,
-			int fromIdx,
-			int toIdx
-	) {
-		if(memoDistances[fromIdx][toIdx] == UNKNOWN) {
-			int[] from = posArray[fromIdx];
-			int[] to = posArray[toIdx];
-			int mDist = Math.abs(to[X] - from[X]) + Math.abs(to[Y] - from[Y]);
-			memoDistances[fromIdx][toIdx] = mDist;
-			memoDistances[toIdx][fromIdx] = mDist;
-		}
-		return memoDistances[fromIdx][toIdx];
+	int[][] buildDistances(int[][] posArray, int numNodes) {
+		int[][] dist = new int[numNodes][numNodes];
+		for (int j=0; j<numNodes; ++j)
+			for (int i=0; i<numNodes; ++i)
+				dist[j][i] = Math.abs(posArray[j][X] - posArray[i][X])
+						+ Math.abs(posArray[j][Y] - posArray[i][Y]);
+		return dist;
 	}
 
-	int dfsVisit(
-			int[][] dist,
-			int[][] arr,
-			boolean[] visited,
-			int N,
-			int current,
-			int depth
-	) { 
-		int minDist = Integer.MAX_VALUE;
-		for(int i=0; i<N+2; i++) {
-			if (visited[i])
-				continue;
-			if (i==1 && depth!=N)
-				continue;
-			if (depth==N) {
-				minDist = getMemoL1Distance(dist, arr, current, HOME);
-				break;
-			}
-			else {
-				visited[i]=true;
-				minDist = Integer.min(
-						getMemoL1Distance(dist, arr, current, i)
-						+ dfsVisit(dist, arr, visited, N, i, depth+1),
-						minDist
-				);
-				visited[i]=false;
-			}
-		}
-		return minDist;
+	boolean isVisited(int mask, int customer) {
+		return (mask & (1 << customer)) != 0;
 	}
-	
+    
+	void fillDp(int[][] dp, int[][] dist, int N) {
+		for (int c=0; c<N; ++c)
+			dp[1 << c][c] = dist[WORK][c+FIRST_CUSTOMER];
+		for (int mask=1; mask<(1 << N); ++mask)
+			for (int last=0; last<N; ++last) {
+				if (dp[mask][last] == INF) continue;
+				int from = last+FIRST_CUSTOMER;
+				for (int i=0; i<N; ++i) {
+					if (isVisited(mask, i)) continue;
+					int next = mask | (1 << i);
+					dp[next][i] = Integer.min(dp[next][i],
+							dp[mask][last] + dist[from][i+FIRST_CUSTOMER]);
+				}
+			}
+	}
+
+	int findAnswer(int[][] dp, int[][] dist, int N) {
+		int full = (1 << N) - 1;
+		int best = INF;
+		for (int i=0; i<N; ++i)
+			best = Integer.min(best,
+					dp[full][i] + dist[i+FIRST_CUSTOMER][HOME]);
+		return best;
+	}
+
 	String solveInner(BufferedReader br) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int numCustomers; // 2≤N≤10
@@ -68,13 +60,12 @@ class Solution {
 			posArray[i][X] = Integer.parseInt(st.nextToken());
 			posArray[i][Y] = Integer.parseInt(st.nextToken());
 		}
-		int[][] dist = new int[numCustomers+2][numCustomers+2];
-		for (int i=0; i<numCustomers+2; ++i)
-			Arrays.fill(dist[i], UNKNOWN);
-		boolean[] visited = new boolean[numCustomers+2];
-		visited[WORK] = true;
-		int minDistance = dfsVisit(dist, posArray, visited, numCustomers, 0, 0);
-		return Integer.toString(minDistance);
+		int[][] dist = buildDistances(posArray, numCustomers+2);
+		int[][] dp = new int[1 << numCustomers][numCustomers];
+		for (int j=0; j<(1 << numCustomers); ++j)
+			Arrays.fill(dp[j], INF);
+		fillDp(dp, dist, numCustomers);
+		return Integer.toString(findAnswer(dp, dist, numCustomers));
 	}
 
 	void solve() throws IOException {
