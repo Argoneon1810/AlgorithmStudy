6a318504c5b70353221751d2197d5c9bc99df142
Jinhoon Choi <62126566+Argoneon1810@users.noreply.github.com>
2026-09-11 11:31:28 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 95 ms, Memory: 27,392 KB -BaekjoonHub

--- Argoneon1810/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ Argoneon1810/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,90 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
-	static int top;
-	static int all;
-	static int[][] groups;
-	static boolean groupIsStale = false;
-	
-	int getInitialBitmask(int N) {
-		int maskHalf = 0b0;
-		int maskAll = 0b0;
-		int bit = 0b1;
-		for (int i=0; i<N; ++i) {
-			maskAll += bit;
-			if (i < N/2)
-				maskHalf += bit;
-			bit <<= 1;
-		}
-		all = maskAll;
-		top = bit;
-		return maskHalf;
+	boolean isUnfillable(int N, int idx, int picked) {
+		return N - idx < N/2 - picked;
 	}
-	
-	int nextBitMask(int N, int prev) {
-		int a = prev & (-prev);
-		int b = prev + a;
-		int c = ((prev ^ b) >> 2) / a;
-		return b | c;
+
+	int dfs(int[] r, int N, int total, int idx, int picked, int sumA) {
+		if (picked == N/2)
+			return Math.abs(2*sumA - total) / 2;
+		if (isUnfillable(N, idx, picked))
+			return Integer.MAX_VALUE;
+		int take = dfs(r, N, total, idx+1, picked+1, sumA + r[idx]);
+		int skip = dfs(r, N, total, idx+1, picked, sumA);
+		return Integer.min(take, skip);
 	}
-	
-	int getComplement(int mask) {
-		return all ^ mask;
-	}
-	
-	void fillGroups(int N, int mask) {
-		if (groups == null || groupIsStale) {
-			groupIsStale = false;
-			groups = new int[2][];
-			groups[0] = new int[N/2];
-			groups[1] = new int[N/2];
-		}
-		for (int i=0, a=0, b=0; i<N; ++i) {
-			int minBit = mask & (-mask);
-			if (minBit==0b1)
-				groups[0][a++] = i;
-			else
-				groups[1][b++] = i;
-			mask >>= 1;
-		}
-	}
-	
-	int getScore(int[][] S, int N, int groupID, int accumScore, int depth) {
-		if (depth >= N/2)
-			return accumScore;
-		int mScore = 0;
-		int[] mGroup = groups[groupID];
-		int left = mGroup[depth];
-		for (int i=depth+1; i<N/2; ++i) {
-			mScore += S[left][mGroup[i]] + S[mGroup[i]][left];
-		}
-		return getScore(S, N, groupID, accumScore + mScore, depth+1);
-	}
-	
-	int evaluate(int a, int b) {
-		return Math.abs(a-b);
-	}
-	
+
 	String solveInner(BufferedReader br) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// (4 ≤ N ≤ 16)
-		int[][] S = new int[N][N];
-		for(int i=0; i<N; ++i) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; ++j) {
-				S[i][j] = Integer.parseInt(st.nextToken());	// (1 ≤ Sij ≤ 20,000, i ≠ j)
+		int N = Integer.parseInt(br.readLine().trim());	// (4 ≤ N ≤ 16)
+		int[] r = new int[N];
+		for (int y=0; y<N; ++y) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int x=0; x<N; ++x) {
+				int s = Integer.parseInt(st.nextToken());
+				r[y] += s;
+				r[x] += s;
 			}
 		}
-		int mask = getInitialBitmask(N);
-		int best = Integer.MAX_VALUE;
-		int half = top >> 1;
-		do {
-			fillGroups(N, mask);
-			int value = evaluate(getScore(S, N, 0, 0, 0), getScore(S, N, 1, 0, 0));
-			best = Integer.min(best, value);
-		} while((mask = nextBitMask(N, mask)) < half);
-		groupIsStale = true;
-		return Integer.toString(best);
+		int total = 0;
+		for (int i=0; i<N; ++i)
+			total += r[i];
+		return Integer.toString(dfs(r, N, total, 1, 1, r[0]));
 	}
 
 	void solve() throws IOException {
